9fed0b69ae94206b66065716e07785b6eaeaa81f
kaushal-1445678 <kaushal730223@gmail.com>
2025-09-17 11:22:50 +0000

    lecture5 about datatypes

---
+++ 01_basics/lecture5_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all js cod ein newer version
+
+// alert(3+3);//we are using node.js but browser
+
+/* datatypes in js is:-
+number =>(2^53)
+bigint
+string
+boolean
+null => standalone value
+undefined
+symbol => unique
+object
+
+console.log(3+3)
+console.log(typeof null);
+console.log(typeof undefined);
