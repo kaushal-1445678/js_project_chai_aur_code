ad14c8ecf3cb038fb3ab08c1bc49e5646e45b890
kaushal-1445678 <kaushal730223@gmail.com>
2026-01-05 19:39:10 +0000

    update data of day2

--- days/day2/day2.js
+++ days/day2/day2.js
@@ -9,15 +9,35 @@ console.log(students);
 students.pop();
 console.log(students);
 
-let number=[1,2,3];
-let result=number.map(function(num){
-    return num*2;
-})
+let number=[1,2,4];
+// let result=number.map(function(num){
+//     return num*2;
+// });
+let result=number.map(num=>num*2);
 console.log(result);
-// let result=number.map(num=>num*2)
 
-let ages=[10,20,15,25];
+
+let ages=[10,20,25];
 let selectAge=ages.filter(function(age){
     return age>=18;
-})
+});
 console.log(selectAge);
+let numbers=[1,2,4,5];
+let sum=numbers.reduce((total,sum)=>{
+    return total+sum;
+},0);
+console.log(sum);
+
+let users=[
+    {
+        name:"kaushal",
+        age:24
+    },
+    {
+        name:"rahul",
+        age:20
+    }
+];
+
+let user=users.find(u=>u.age >=21);
+console.log(user);
