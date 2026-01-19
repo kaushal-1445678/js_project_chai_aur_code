0951ad9f45270f03ecbf4fffc5101a9be317b870
kaushal-1445678 <kaushal730223@gmail.com>
2026-01-19 13:59:51 +0000

    update day3 content

--- days/day3/day3.js
+++ days/day3/day3.js
@@ -1,63 +1,70 @@
-//spread operator
+// //spread operator
 
-//example 1
-let num1=[1,2];
-let num2=[...num1,3,4];
-console.log(num2);
+// //example 1
+// let num1=[1,2];
+// let num2=[...num1,3,4];
+// console.log(num2);
 
-//example 2
-let users={
-    name:'kaushal',
-    age:24
-};
-let user={
-    ...users,
-    city:'Agra'
-}
-console.log(user);
-
-
-//Rest Operator
-
-function add(...numbers){
-    return numbers.reduce((sum,n)=>sum+n,0);
-}
-console.log(add(1,2,4));
+// //example 2
+// let users={
+//     name:'kaushal',
+//     age:24
+// };
+// let user={
+//     ...users,
+//     city:'Agra'
+// }
+// console.log(user);
 
 
+// //Rest Operator
+// // What ...numbers means (Rest Parameter)
 
-//Destructuring
-//object destructuring
+// // The ... in a function parameter is called the rest parameter.
+// // It collects all remaining arguments passed to the function into a real array.
+// // In this case, numbers becomes an array of all the values you pass to add.
 
-let userone = {
-    name:'kaushal',
-    age:24
-}
-let {name,age}=userone;
-console.log(name);
-console.log(age);
+// // Example:
+
+// function add(...numbers){
+//     return numbers.reduce((sum,n)=>sum+n,0);
+// }
+// console.log(add(1,2,4));
 
 
-//array destructuring
-let usertwo=["Red","blue"]
+
+// //Destructuring
+// //object destructuring
+
+// let userone = {
+//     name:'kaushal',
+//     age:24
+// }
+// let {name,age}=userone;
+// console.log(name);
+// console.log(age);
+
+
+// //array destructuring
+// let usertwo=["Red","blue"]
    
-let [first,second]=usertwo;
-console.log(first);
-console.log(second);
+// let [first,second]=usertwo;
+// console.log(first);
+// console.log(second);
 
 
-//JSON-- Data format used in API's
-let user4={
-    name:'kaushal',
-    age:24
-}
+// //JSON-- Data format used in API's
+// let user4={
+//     name:'kaushal',
+//     age:24
+// }
 
-//to convert in JSON
-let obj=JSON.stringify(user4);
+// //to convert in JSON
+// let obj=JSON.stringify(user4);
 
-// console.log(user4);
-console.log(obj);
+// // console.log(user4);
+// console.log(obj);
 
-//convert back to object
-let obj2=JSON.parse(obj);
-console.log(obj2);
+// //convert back to object
+// let obj2=JSON.parse(obj);
+// console.log(obj2);
